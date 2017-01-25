f7207485e53c6085d143f50e24fd8855c749d509
Githubbinaryhelp <github@binaryhelp.com>
2017-01-25 18:56:33 -0500

    Add files via upload

--- Message.Add1.php
+++ Message.Add1.php
@@ -3,16 +3,27 @@
     position: fixed;
     bottom: 0;
     right: 0;
+    margin:0;
+    border:0px;
+  }
+  #Transparent
+  {
+  min-width:300px; 
+  max-width:300px;
+  width: 300px;
+  background: none;
+  border: 0px;
   }
 </style>
 
 <? session_start();
+
   echo"<form action='Message.Add2.php' method='post'>";     
   echo "<table id='MessageBottomRight' class='table table-bordered table-hover'>";
   echo "<tr>";
-  //echo "<td></td>";<div class='form-group'></div>
-  echo "<td><input type='text' class='form-control' name='Message' value='new message'></td>";
-  echo "<td><input type='submit'  class='form-control' value='Save' ></td>";
+  echo "<td id='Transparent'>&nbsp;</td>";
+  echo "<td style='background-color: #fed600;'><input type='text' name='Message' value=''><input type='submit' value='Save' ></td>";
+  //echo "<td></td>";
   echo "</tr>";
   echo "</tbody>";
   echo "</table>";
