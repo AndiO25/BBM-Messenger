fa07e952155a3896eed3dcdfe4bf2eb524fc88e0
Andres Ortega <AndiO@AndiOs-MacBook.local>
2017-01-25 21:11:32 -0500

    yellow text box

--- Message.Add1.php
+++ Message.Add1.php
@@ -13,16 +13,17 @@
   width: 300px;
   background: none;
   border: 0px;
+
   }
 </style>
 
 <? session_start();
 
   echo"<form action='Message.Add2.php' method='post'>";     
-  echo "<table id='MessageBottomRight' class='table table-bordered table-hover'>";
+  echo "<table id='MessageBottomRight' class='table table-bordered'>";
   echo "<tr>";
   echo "<td id='Transparent'>&nbsp;</td>";
-  echo "<td style='background-color: #fed600;'><input type='text' name='Message' value=''><input type='submit' value='Save' ></td>";
+  echo "<td style='background-color: #fed600; '> <textarea class='form-control' style='display:inline; width:90%; max-width:90%; vertical-align:middle;' rows='2' name='Message' value=''></textarea> <input type='submit' value='Send' class='btn btn-default btn-lg'></td>";
   //echo "<td></td>";
   echo "</tr>";
   echo "</tbody>";
