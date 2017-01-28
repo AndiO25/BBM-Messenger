48cf8713e0076a5e511526f30cfedaf08adaa87c
Githubbinaryhelp <github@binaryhelp.com>
2017-01-28 12:20:08 -0500

    Add files via upload

---
+++ CheckSessionVariables.php
@@ -0,0 +1,17 @@
+<? session_start();
+
+//30	Bugs_Bunny@looneytunes.tv	
+//31	Daffy_Duck@looneytunes.tv
+//32	Yosemite_Sam@looneytunes.tv	
+//33	Elmer_Fudd@looneytunes.tv
+echo "UserID:".$_SESSION['UserID'];
+echo "<br>";
+echo "Full_Name:".$_SESSION['Full_Name'];
+echo "<br>";
+echo "EmailAddress:".$_SESSION['EmailAddress'];
+echo "<br>";
+echo "UserImage:".$_SESSION['UserImage'];
+echo "<br>";
+
+                
+?>
